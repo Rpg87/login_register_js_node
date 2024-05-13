b84551a25249d1ecda0d081e4fd100f8d6c7bf18
Rpg87 <sra.picadillo@gmail.com>
2024-05-13 11:15:41 +0200

    add styles and pages for each process

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-'use stric';
+'use strict';
 
 import express from 'express';
 //Fix for __dirname
@@ -6,12 +6,19 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+
+
 // Server
 const app = express();
 app.set('port', 4000);
 app.listen(app.get('port'));
 console.log('Servidor funcionando en ', app.get('port'));
 
+//Configuration
+app.use(express.static(__dirname + '/public'));
+
 //Routes
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/pages/login.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/pages/login.html'));
+app.get('/register', (req, res) => res.sendFile(__dirname + '/pages/register.html'));
+app.get('/admin', (req, res) => res.sendFile(__dirname + '/pages/admin/admin.html'));
